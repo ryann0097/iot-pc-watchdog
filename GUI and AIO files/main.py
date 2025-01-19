cb7061be1b201ed22227bb219fbd3bd5ac5a726d
Ryan Silvestre <129810642+ryann0097@users.noreply.github.com>
2025-01-18 19:21:27 -0600

    feat: gui and io files branch added

---
+++ GUI and AIO files/main.py
@@ -0,0 +1,24 @@
+# Primeiramente, vamos iniciar a interface.
+# Depois setaremos uma instância única da classe usuário
+
+# Primeira parte
+# Login ou registro
+
+# Ações que precisam do banco de dados:
+# Login:
+# Usuário insere PID (único)
+## --> Ação necessária: consulta no banco de dados (validação)
+
+
+# Registro:
+# Usuário insere PID (único)
+# Usuário insere nome
+## Ação necessária: escrita no banco de dados (criação)
+
+# Ações principais
+
+
+
+
+
+
